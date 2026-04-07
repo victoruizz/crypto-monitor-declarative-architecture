d73d89bff658846fd418ce16a794262aa5415332
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-07 19:38:41 -0300

    first commit

---
+++ app/src/main/java/com/github/victoruizz/crypto_monitor_declarative_architecture/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.github.victoruizz.crypto_monitor_declarative_architecture
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.victoruizz.crypto_monitor_declarative_architecture.ui.theme.CryptomonitordeclarativearchitectureTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CryptomonitordeclarativearchitectureTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CryptomonitordeclarativearchitectureTheme {
+        Greeting("Android")
+    }
+}
